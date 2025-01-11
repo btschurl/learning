53710522d7a4efd18e978b112b36a67cef0d6616
btschurl <btschurl@icloud.com>
2025-01-11 02:23:49 +0100

    Added fetch testing project

---
+++ js/testing-fetch/app.js
@@ -0,0 +1,25 @@
+// https://api.chucknorris.io/jokes/random
+
+const chuckNorrisRandomJoke = async () => {
+    const url = "https://api.chucknorris.io/jokes/random";
+    try {
+        const res = await fetch(url);
+        if(!res.ok) {
+            throw new Error(`Response status: ${res.status}`);
+        }
+        const data = await res.json();
+        return data.value;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const jokeBox = document.querySelector(".joke");
+const jokeButton = document.querySelector(".jokeRandom");
+const jokeAuthor = document.querySelector(".jokeAuthor");
+
+jokeButton.addEventListener("click", async () => {
+    const joke = await chuckNorrisRandomJoke();
+    jokeBox.textContent = joke;
+    jokeAuthor.classList.add("visible");
+});
